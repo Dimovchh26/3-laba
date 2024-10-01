fefd6cf82936d672191d529527acb8ad87c4bf7a
yanakorobeinikova <yanaa.rayy@gmail.com>
2024-10-01 16:18:22 +0300

    first

---
+++ chetvertoe.bat
@@ -0,0 +1,5 @@
+@echo off
+set /P bur1=enter papka
+set /P bur2=enter papka
+xcopy %bur1% %bur2% /S
+pause
---
+++ sedmoe.bat
@@ -0,0 +1,4 @@
+@echo off
+git config --global user.name "Polina00000976 "
+git config --global user.email "polinaburmi04@icloud.com "
+pause
